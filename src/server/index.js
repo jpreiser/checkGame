6c63660db410fb4f9fe288e208ec1f7548bf88a8
Jacob Reiser <jacobreiser@Jacobs-Mac-mini.local>
2023-11-03 15:36:48 -0400

    Cleaning up, basic server created

--- src/server/index.js
+++
@@ -1,9 +0,0 @@
-const express = rquire("express");
-
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-app.listen(PORT, () => {
-    console.log('Server listening on ${PORT}');
-});
